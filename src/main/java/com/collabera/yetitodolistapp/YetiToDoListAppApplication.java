c282120c6145bc19ea4c40601807ae8a3842f207
Your Name <wamontgomery0068@gmail.com>
2019-09-04 08:37:29 -0500

    first commit

---
+++ src/main/java/com/collabera/yetitodolistapp/YetiToDoListAppApplication.java
@@ -0,0 +1,13 @@
+package com.collabera.yetitodolistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetiToDoListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetiToDoListAppApplication.class, args);
+	}
+
+}
